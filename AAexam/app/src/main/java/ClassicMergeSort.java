5989796bf76c338dfae504554a9244e16da3f501
Marcus Skjold <marcusskjold@gmail.com>
2024-12-05 11:36:00 +0100

    Start classic mergesort

---
+++ AAexam/app/src/main/java/ClassicMergeSort.java
@@ -0,0 +1,23 @@
+
+public class ClassicMergeSort {
+
+    public static <T extends Comparable<? super T>> int sort(T[] a) {
+        T[] aux = a.clone();
+        sort(a, 0, a.length - 1);   
+        return -1;
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) { 
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo)/2;
+        sort(a, lo, mid);
+        sort(a, mid+1, hi);
+        merge(a, lo, mid, hi);
+    } 
+
+    /* Sanity checks */
+    public static void main(String[] args) {
+
+        
+    }
+}
