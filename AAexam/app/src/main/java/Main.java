801046e5612d209276f8cd262dba8dfec1edd2c2
Marcus Skjold <marcusskjold@gmail.com>
2024-12-09 19:19:21 +0100

    Implement initial testing framework.
    I compared it to the benchmark class from PCPP (see Main class),
    and got similar results (The first result is a simple multiplication,
    the second my benchmark, the third benchmark from PCPP):
    
    ```output
    OS: Mac OS X; 15.0; aarch64
    JVM: Eclipse Adoptium; 21.0.5
    CPU: null; 8 "procs"
    Date: 2024-12-09T19:15:40+0100
    
    title                  param                mean        stdev         runs         reps
    multiply                                      23.0         0.70           10   16,777,216
    randomIntSort              100            12,587.6        27.11            5       32,768
    randomIntSort              200            29,510.7        92.83            5       16,384
    randomIntSort              400            66,551.6       124.60            5        4,096
    randomIntSort              800           151,462.4       301.16            5        2,048
    randomIntSort             1600           334,960.6       288.17            5        1,024
    randomIntSort             3200           736,375.0       830.83            5          512
    randomIntSort             6400         1,610,642.2     1,195.47            5          256
    randomIntSort            12800         3,531,779.9     3,539.04            5          128
    randomIntSort            25600         7,840,721.1    10,730.80            5           32
    randomIntSort            51200        17,335,303.1   105,661.48            5           16
    rIntSort                       100        12666.8 ns      34.79      32768
    rIntSort                       200        30372.3 ns      90.97      16384
    rIntSort                       400        68057.7 ns     126.48       4096
    rIntSort                       800       153305.3 ns    4756.26       2048
    rIntSort                      1600       331004.6 ns    1977.07       1024
    rIntSort                      3200       724461.1 ns     568.69        512
    rIntSort                      6400      1560474.5 ns   11939.01        256
    rIntSort                     12800      3429757.3 ns   10457.52        128
    rIntSort                     25600      7542085.7 ns   23135.12         64
    rIntSort                     51200     16369096.9 ns  120910.94         16
    rIntSort                    102400     34253957.2 ns  131978.89          8
    rIntSort                    204800     74598337.6 ns  299176.61          4
    rIntSort                    409600    164452925.2 ns 1143026.61          2
    rIntSort                    819200    484876593.7 ns 10566957.10          2
    rIntSort                   1638400   1139184843.8 ns 13881556.76          2
    
    ```

--- AAexam/app/src/main/java/Main.java
+++ AAexam/app/src/main/java/Main.java
@@ -1,5 +1,53 @@
+import static experiments.Benchmark.*;
+import benchmarking.*;
+
+import java.util.Map;
+import java.util.function.IntFunction;
+
+import data.Handler;
+import sorting.TopDownMergeSort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double timeLimit = 0.25;
+        systemInfo();
+        System.out.println();
+        System.out.printf( "%-20s%10s%20s%13s%13s%13s%n", 
+            "# title", "param", "mean", "stdev", "runs", "reps"
+        );
+
+        printResult(
+            "multiply",
+            performExperiment(10, 0.25, new Experiment<Integer>(i -> i, experiments.Benchmark::multiply, i -> i))
+        );
+        
+
+        // ===========
+
+        IntFunction<Experiment<Integer[]>> gen = i -> {
+            Integer[] data = Handler.generate(i, j -> j);
+            return new Experiment<Integer[]>(j -> data, TopDownMergeSort::sort, Handler::randomize);
+        };
+
+        Map<Integer, Result> x = parameterizedExperiment(100, 100_000, 2, 5, timeLimit, gen);
+
+        x.forEach((k,v) -> printResult("randomIntSort", v, k));
+
+
+        // ================= 
+
+
+        for (int size = 100; size <= 2_000_000; size *= 2) { 
+            final Integer[] intArray = Handler.generate(size, i -> i);
+            Benchmark.Mark8Setup(
+                "rIntSort", 
+                String.format("%8d", size),
+                new Benchmarkable() {
+                    public void setup() { Handler.inPlaceRandomize(intArray);}
+                    public double applyAsDouble(int i) {
+                        return (double) TopDownMergeSort.sort(intArray);
+                    }
+                });
+        }
     }
 }
