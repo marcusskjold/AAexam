2fa62c9669d8bb3af7c32199d6d00d5a9a0d7e17
Andreas Riget Bagge <anrb@itu.dk>
2024-12-12 13:44:58 +0100

    implement bitwise version of it mergesort
    make tests and it passes all. Might need more. Also has an adhoc check to avoid redundant merge in the first merge of final merging

--- AAexam/app/src/main/java/sorting/BottomUpMergeSort.java
+++ AAexam/app/src/main/java/sorting/BottomUpMergeSort.java
@@ -7,8 +7,8 @@ public class BottomUpMergeSort {
     private BottomUpMergeSort() {}
 
 
-    //TODO: maybe T extends comparable necessary here?
-    //TODO: could bit-operations and the like be utilized here, e.g. in modelling the stack, lengths etc.?
+    //Version of sort that uses a stack data-structure, rather than a binary number
+    //Have only been checked by a few tests
     private static  int sort(Comparable[] a, Comparable[] aux) {
         /*Creates empty stack for runs
         For now runs are length 2 int-arrays where int[0] is the leftmost index in array a,
@@ -71,11 +71,12 @@ public class BottomUpMergeSort {
         //debug:
         System.out.println("Resulting array: " + Arrays.toString(a));
     
-        //TODO: handle counting compares
         return 0;
     }
 
 
+
+    
     /**
      * Rearranges the array in ascending order, using the natural order
      * @param a the array to be sorted
@@ -83,8 +84,90 @@ public class BottomUpMergeSort {
      */
     public static int sort(Comparable[] a) {
         Comparable[] aux = new Comparable[a.length];
-        int compares = sort(a, aux);
+        int compares = sortBitWise(a, aux);
         assert SortUtils.isSorted(a);
+        return compares;
+    }
+
+
+    //TODO: count compares (can just be done at merge, I think?)
+    private static int sortBitWise(Comparable[] a, Comparable[] aux) {
+        //counter for performed compares
+        int compares = 0;
+        //length of array
+        int n = a.length;
+        //If 1 or fewer elements in array, no need to sort
+        if(n<=1) return compares;
+
+        //Represents the stack of runs as an int, with a set bit corresponding to the length of a run.
+        //The rightmost bit then represents the top of the stack
+        //For instance ...1101 would correspond to a run of length 1 in the top, followed by a run of length 4, and a run of length 8
+        //This utilizes the fact that the run lengths decreases by a factor of 2, when going up the stack
+        int runStack = 0;
+
+        //Iterate through array and compute runs
+        for(int i=0;i<n;i++) {
+            //sets length of next run to 1
+            int runLength = 1;
+            //Iterate through bits of the stack (leftshifting runLength for each succes)
+            //till next bit not set as well.
+            //1: While next element in stack is same length as current element
+            while((runStack & runLength) != 0) {
+                //define initial index for run to merge with (the lengths of the runs minus the one to merge with)
+                int lo = runStack - runLength;
+                //compute mid as the final index of the next run in stack (as usual)
+                int mid = runStack - 1;
+                //define final index as the sum of lengths and current run minus 1
+                int hi = runStack + runLength - 1;
+                //debug
+                System.out.println("Now merging: a[" + lo + " .. " + mid + "] + a [" + mid + " + 1 .. " + hi + "]");
+                //merge current run with next run (and count compares):
+                compares += Merge.merge(a, aux, lo, mid, hi);
+                //remove the next (now merged) run from stack (by bitwise AND with reverse of runLength)
+                runStack = runStack & (~runLength);
+                //double length of current run:
+                runLength = runLength << 1;
+            }
+            //when not a next run of same length, put current run in stack (using bitwise or):
+            runStack = runStack | runLength;
+        }
+        //When stack iterated through, merge rest of runs in stack till just one run:
+        //define counter for iterating through stack, starting at lowest possible length (first bit)
+        int runLength = 1;
+        //define final index as the last index in the array (since upmost merged, this will stay the same)
+        int hi = n - 1;
+
+        //if(runStack == n) {
+        //    runStack = runStack & (~runLength);
+        //    runLength = runLength << 1;
+        //}
+        //Idea: Manually see if first merge necessary first element?
+
+        System.out.println("Finishing up stack:");
+        //while still elements in stack:
+        while(runStack != 0) {
+            //If run of current length found (can only be one for each length pr the stack invariant)
+            if((runStack & runLength) != 0) {
+                //define lower bound
+                int lo = runStack - runLength;
+                //compute mid (as usual)
+                int mid = runStack - 1;
+                //If not a redundant merge: //TODO: verify this check (Maybe a bit ad hoc)
+                if(hi >= mid + 1) {
+                    //debug
+                    System.out.println("Now merging: a[" + lo + " .. " + mid + "] + a [" + mid + " + 1 .. " + hi + "]");
+                    //merge current run with next run (and count compares):
+                    compares += Merge.merge(a, aux, lo, mid, hi);
+                }
+                //remove the next (now merged) run from stack
+                runStack = runStack & (~runLength);
+            }
+            //proceed to next bit (length)
+            runLength = runLength << 1;
+        }
+        //debug
+        System.out.println(Arrays.toString(a));
+
         return compares;
     }
 
