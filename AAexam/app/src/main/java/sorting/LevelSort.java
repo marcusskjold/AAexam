9c6a247073b5d3597b78139891938ae02e119635
Andreas Riget Bagge <anrb@itu.dk>
2024-12-16 15:45:57 +0100

    make correct merging logic in LvlSort
    make rest of tests for non-adaptible version and cleaned up in code. lvlSort now uses a min-operation in first loop. Even though this might be a bit more expensive, it ensures correct merging logic and greatly simplifies the algorithm

--- AAexam/app/src/main/java/sorting/LevelSort.java
+++ AAexam/app/src/main/java/sorting/LevelSort.java
@@ -28,61 +28,59 @@ public class LevelSort {
     
     //Trying to create non-adaptive variant (always create new runs of length c)
     private static int sort(Comparable[] a, Comparable[] aux, int c) {
+
         //Counter for compare and length defined
         int compares = 0;
         int n = a.length;
-        //check if any compares necessary at all
+
+        //check if sorting necessary at all
         if(n<=1) return compares;
 
         //define capacity for stacks of runs, indexed by level
-        //Can't exceed the position of the most significant bit of the sum of last two entries (max level) +1 (accounting for that first index is 0 rather than 1)
-        int stackCapacity = 64 - Long.numberOfLeadingZeros(((long) (n - 1) + (long) (n - 2))) + 1;
-        //int stackCapacity = (int)(Math.log(n) / Math.log(2)) + 2;
+        //Can't exceed the position of the most significant bit of the sum of n and n - 1 (max level) +1 (accounting for that first index is 0 rather than 1)
+        int stackCapacity = 64 - Long.numberOfLeadingZeros(((long) n + (long) (n - 1))) + 1;
 
         //create stacks for determining start and ends of runs, depending on their power
         //Should work as level-stack is increasing monotonically and no consecutive runs are equal
         int[] runStart = new int[stackCapacity];
         int[] runEnd = new int[stackCapacity];
         
-        //stack maintaining levels (Might turn out to be better with counter)
+        //stack maintaining levels
         int levelStack = 0;
 
-        //TODO: maybe defining this initially might just be necessary for adaption?
-
-
-        //TODO: should any start and end points in array be updated here?
         //create initial run from 0 of length c (or lesser if constrained by array size)
         //This is our first merging candidate (L), which will immediately be put in stack
         int startL = 0;
         int endL = Math.min(startL+c-1, n-1);
         compares += InsertionSort.sort(a, startL,endL);
 
-        //Start and end index for next run, N, to compare with
-        //Is initially set to start and end of L in case of just one run in whole stack (to be used below this loop)
-        int startN= startL; //TODO: okay?
-        int endN = endL;
-
         //starting from the end of first run
         //iterates through array in intervals of length c, run by run
-        for(int i=endL+1; i+c-1<n; i+=c) {
-            //find new run N of length c: 
-            startN = i;
-            endN = startN+c-1;
+        //while the end of the array hasn't been reached
+        while(endL < n - 1) {
+
+            //find new run N of length c (or less, if constrained by array length): 
+            int startN = endL + 1;
+            int endN = Math.min(startN+c-1, n - 1);
             compares += InsertionSort.sort(a,startN,endN);
+
             //compute level of boundary between run L and run N
             int currentLevel =  level(startL, endL, startN, endN);
 
-
-            //update end-point of run L (its level is the level of right boundary):
-            //To be used for potential merge (?)
+            //update end-point of run L in end-stack, corresponding to its power
+            //(its level is the level of right boundary):
+            //To be used for potential merges in stack (if top level < currentLevel)
             runEnd[currentLevel] = endL;
             
             //If this level greater than level of run at top of stack (which must be the position of the LSB, as stack is monotonic):
             //also won't be equal since no consecutive boundaries have same level
             //1: while levelStack is non-empty (!=0), and current level bigger than top level in stack
             while(levelStack!=0 && currentLevel > Integer.numberOfTrailingZeros(levelStack) + 1) {
-                //define level of the run in top of the stack (must be position of LSB, due to stack invariant of monotonic and decreasing):
+
+                //define level of the run in top of the stack 
+                //(must be position of LSB, due to stack invariant of monotonic and decreasing):
                 int topLevel = Integer.numberOfTrailingZeros(levelStack) + 1;
+
                 //merge L with run in top of stack
                 int lo = runStart[topLevel];
                 int mid = runEnd[topLevel];
@@ -90,34 +88,29 @@ public class LevelSort {
                 //debug
                 System.out.println("Now merging: a[" + lo + " .. " + mid + "] + a [" + mid + " + 1 .. " + hi + "]");
                 compares += Merge.merge(a, aux, lo, mid, hi);
-                //remove the run from the levelStack (by AND with 1 leftshifted by toplevel - 1) (and update starting-point of L)
+                //remove the (now merged) top run from the levelStack (by AND with 1 leftshifted by toplevel - 1) 
                 levelStack &= ~(1 << (topLevel - 1));
-                //runStart[currentLevel] = lo;
+                //update starting-point of L (according to merge)
                 startL = lo;
             }
+
             //When l > currentLevel, put L on stack (Bitwise or with 1 leftshifted lvl -1 times), and set start of run (updated in loop if entered):
-            levelStack|=1 << (currentLevel -1 );
-            //Maybe assert that checks that the level stack is a power of two?
+            levelStack|= 1 << (currentLevel - 1);
+            //Update start of run in stack with the level of L (only one with this level pr. stack property)
             runStart[currentLevel] = startL;
+
             //Set N as the new L
             startL = startN;
             endL = endN;
         }
-        //if run with length < c left (no runs of length >=c should be left after this iteration)
-        if(endN + 1 < n) {
-            //TODO: If residue, it should merge with run N (indices correct)?
-            int lo = startN;
-            int mid = endN;
-            //get indeces for residual run and sort it
-            startN = endN + 1;
-            endN = n - 1;
-            compares += InsertionSort.sort(a, startN,endN);
-            Merge.merge(a,aux,lo,mid, endN);
-            
-        }
+        
+        //debug:
+        System.out.println("finishing stack:");
+        
         //set endpoint for finishing merges (will just be right end of array)
-        int hi = endN;
+        int hi = endL;
         //Then pop elements from stack one by one, and merge it with top run:
+        //(Has less explanation, as this is largely a simpler version of the initial iteration)
         while(levelStack !=0) {
             int topLevel = Integer.numberOfTrailingZeros(levelStack) + 1;
             int lo = runStart[topLevel];
@@ -125,11 +118,9 @@ public class LevelSort {
             compares += Merge.merge(a, aux, lo, mid, hi);
             //debug
             System.out.println("Now merging: a[" + lo + " .. " + mid + "] + a [" + mid + " + 1 .. " + hi + "]");
-            //remove the run from the levelStack (and update starting-point of L)
-            //TODO: remove toplevelth bit rather than toplevel, as such
+            //remove the run from the levelStack
             levelStack &= ~(1 << (topLevel - 1));
         }
-        
         
         //debug
         System.out.println(Arrays.toString(a));
--- AAexam/app/src/test/java/LevelSortTest.java
+++ AAexam/app/src/test/java/LevelSortTest.java
@@ -1,19 +1,60 @@
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-import sorting.BottomUpMergeSort;
-import sorting.BottomUpMergeSortCutoff;
+import data.Handler;
+import data.TestData;
 import sorting.LevelSort;
 
 public class LevelSortTest {
+
     //-----------------------------------
-    //Case: Descending order rather than ascending
-    //Also: just one run in stack after initial computation of runs
+    //Case: Invalid cutoff value
+    //-----------------------------------
+    @Test
+    void givenInvalidCutoffvalue_whenLevelSort_thenThrowsException() {
+        Exception e = assertThrows(IllegalArgumentException.class, 
+        () -> LevelSort.sort(new Integer[]{1,2,3},-2));
+        assertEquals("Cutoff value must be at least 1.", e.getMessage());
+    }
+
+    //-----------------------------------
+    //Case: Empty array
+    //-----------------------------------
+    @Test
+    void givenEmptyArray_LevelSort_thenEmptyArray() {
+        Integer[] emptyArray = new Integer[0];
+        LevelSort.sort(emptyArray,1);
+        assertArrayEquals(new Integer[]{}, emptyArray);
+    }
+
+    @Test
+    void givenEmptyArray_LevelSort_thenReturnNumberOfCompares() {
+        Integer[] emptyArray = new Integer[0];
+        assertEquals(0, LevelSort.sort(emptyArray,1));
+    }
+
+    //-----------------------------------
+    //Case: Only one element
+    //-----------------------------------
+    @Test
+    void givenSizeOneArray_whenLevelSort_thenIdenticalArray() {
+        Integer[] sizeOneArray = new Integer[]{1};
+        LevelSort.sort(sizeOneArray,1);
+        assertArrayEquals(new Integer[]{1}, sizeOneArray);
+    }
+
+    @Test
+    void givenSizeOneArray_whenLevelSort_thenReturnNumberOfCompares() {
+        Integer[] sizeOneArray = new Integer[]{1};
+        assertEquals(0, LevelSort.sort(sizeOneArray,1));
+    }
+
+    //-----------------------------------
+    //Case: Just one run put in stack during initial iteration
     //------------------------------------
-    //Note: succesfully puts a run in the stack and subsequently merges with second, last run (that wasn't put in stack first)
-    //"Also" no residues
     @Test
     void givenDescendingArray_LevelSortc2_thenAscendingArray() {
         Integer[] descendingArray = new Integer[]{2,1,0,-1};
@@ -21,14 +62,14 @@ public class LevelSortTest {
         assertArrayEquals(new Integer[]{-1,0,1,2}, descendingArray);
     }
 
-    //@Test
-    //void givenDescendingArray_whenMergeSortCutoff_ReturnNumberOfCompares() {
-    //    Integer[] descendingArray = new Integer[]{2,1,0,-1};
-    //    assertEquals(4, LevelSort.sort(descendingArray, 2));
-    //}
+    @Test
+    void givenDescendingArray_LevelSortc2_thenReturnNumberOfCompares() {
+        Integer[] descendingArray = new Integer[]{2,1,0,-1};
+        assertEquals(4, LevelSort.sort(descendingArray, 2));
+    }
 
     //-----------------------------------
-    //Case: Odd amount of elements
+    //Case: array length not evenly divisible by run lengths (last run will not be of length c)
     //-----------------------------------
     @Test
     void givenOddSizeArray_whenLevelSortc3_thenSortsArray() {
@@ -37,17 +78,137 @@ public class LevelSortTest {
         assertArrayEquals(new Integer[]{0,1,2,3,4,5,6,7,8,9,10}, oddSizeArray);
     }
 
+    @Test
+    void givenOddSizeArray_whenLevelSortc3_thenReturnNumberOfCompares() {
+        Integer[] oddSizeArray = new Integer[]{9,1,2,0,7,4,3,8,5,6,10};
+        assertEquals(33, LevelSort.sort(oddSizeArray, 3));
+    }
 
-    //    //case: different cutoff-values give different comparrison numbers
-    //@Test
-    //void givenOddSizeArray_whenMergeSortCutoff3_thenReturnSameComparesAsNoCutoff() {
-    //    Integer[] oddSizeArray = new Integer[]{9,1,2,0,7,4,3,8,5,6,10};
-    //    assertEquals(29, LevelSort.sort(oddSizeArray,3));
-    //}
-        
-    //@Test
-    //void givenOddSizeArray_whenMergeSortCutoff5_thenReturnSameComparesAsNoCutoff() {
-    //    Integer[] oddSizeArray = new Integer[]{9,1,2,0,7,4,3,8,5,6,10};
-    //    assertEquals(33, LevelSort.sort(oddSizeArray,5));
-    //}
+    //(Changing value of c to check that order of merging follows accordingly)
+    @Test
+    void givenOddSizeArray_whenLevelSortc4_thenReturnNumberOfCompares() {
+        Integer[] oddSizeArray = new Integer[]{9,1,2,0,7,4,3,8,5,6,10};
+        assertEquals(29, LevelSort.sort(oddSizeArray, 4));
+    }
+
+
+    //-----------------------------------
+    //Case: Highest possible level relative to stack-size computed (Five elements with c= 1) 
+    // n + (n-1) yields higher level than (n - 1) + (n - 2)
+    //-----------------------------------
+
+    @Test
+    void givenSize5Array_whenLevelSortc1_thenSortsArray() {
+        Integer[] size5Array = new Integer[]{4,3,2,1,0};
+        LevelSort.sort(size5Array, 1);
+        assertArrayEquals(new Integer[]{0,1,2,3,4}, size5Array);
+    }
+
+    //-----------------------------------
+    //Case: Two runs in stack after initial iteration.
+    //In initial iteration, both a merge going to end of stack
+    //and a merge stopped by run with larger level
+    //-----------------------------------
+    @Test
+    void givenSize7Array_whenLevelSortc1_thenSortsArray() {
+        Integer[] size7Array = new Integer[]{7,6,5,4,3,2,1};
+        LevelSort.sort(size7Array, 1);
+        assertArrayEquals(new Integer[]{1,2,3,4,5,6,7}, size7Array);
+    }
+
+    @Test
+    void givenSize7Array_whenLevelSortc1_thenReturnNumberOfCompares() {
+        Integer[] size7Array = new Integer[]{7,6,5,4,3,2,1};
+        assertEquals(9, LevelSort.sort(size7Array, 1));
+    }
+
+    //-----------------------------------
+    //Case: One run in stack after initial iteration
+    //-----------------------------------
+    @Test
+    void givenSize12Array_whenLevelSortc3_thenSortsArray() {
+        Integer[] size12Array = new Integer[]{4,3,2,1,1,2,3,4,4,3,2,1};
+        LevelSort.sort(size12Array, 3);
+        assertArrayEquals(new Integer[]{1,1,1,2,2,2,3,3,3,4,4,4}, size12Array);
+    }
+
+    //-----------------------------------
+    //Case: No merges during initial iteration
+    //stack will just contain two runs of lvl 3 and 4, respectively
+    //-----------------------------------
+    @Test
+    void givenSize9Array_whenLevelSortc3_thenSortsArray() {
+        Integer[] size9Array = new Integer[]{4,3,2,1,1,2,3,4,4};
+        LevelSort.sort(size9Array, 3);
+        assertArrayEquals(new Integer[]{1,1,2,2,3,3,4,4,4}, size9Array);
+    }
+
+    @Test
+    void givenSize9Array_whenLevelSortc3_thenReturnNumberOfCompares() {
+        Integer[] size9Array = new Integer[]{4,3,2,1,1,2,3,4,4};
+        assertEquals(17, LevelSort.sort(size9Array, 3));
+    }
+
+
+    //-----------------------------------
+    //Case: all elements equal value (using Testdata tuples with identical values and distinct id's)
+    //As such, if not identical would imply that some tuples with equal value were switched (e.g. try value = -i)
+    //-----------------------------------
+    @Test
+    void givenIdenticalElementsArray_whenLevelSort_thenPreserveStability() {
+        TestData[] identicalElementsArray = Handler.generate(10, i -> new TestData(i,0));
+        LevelSort.sort(identicalElementsArray,4);
+        assertArrayEquals(Handler.generate(10, i -> new TestData(i,0)), identicalElementsArray);
+    }
+
+    @Test
+    void givenIdenticalElementsArray_whenLevelSort_thenReturnNumberOfCompares() {
+        TestData[] identicalElementsArray = Handler.generate(10, i -> new TestData(i,0));
+        assertEquals(19, LevelSort.sort(identicalElementsArray,4));
+    }
+
+    //-----------------------------------
+    //Case: small array contains duplicates (for easier readability compared to medium array)
+    //-----------------------------------
+    @Test
+    void givenDuplicateElementsArray_whenLevelSort_thenSortsArrayStably() {
+        TestData[] duplicateElementsArray = Handler.readData(Handler.streamFile("unittest/duplicateElems.TestData.in"), 
+        TestData::from); 
+        TestData[] expectedArray = Handler.readData(Handler.streamFile("unittest/duplicateElems.TestData.out"), 
+        TestData::from); 
+        LevelSort.sort(duplicateElementsArray,2);
+        assertArrayEquals(expectedArray, duplicateElementsArray);
+    }
+
+    @Test
+    void givenDuplicateElementsArray_whenLevelSort_thenReturnNumberOfCompares() {
+        TestData[] duplicateElementsArray = Handler.readData(Handler.streamFile("unittest/duplicateElems.TestData.in"), 
+        TestData::from); 
+        assertEquals(9, LevelSort.sort(duplicateElementsArray,2));
+    }
+
+
+    //-----------------------------------
+    //Case: medium array contains duplicates
+    //-----------------------------------
+    //TODO: Important! When one of the files are modified, it doesn't seem to be updated in the read array (a cache seems to be used) (Maybe okay and because it has to be built also?)
+    //changing name and back can solve this ish
+    @Test
+    void givenDuplicateElementsArrayMedium_whenLevelSort_thenSortsArrayStably() {
+        TestData[] duplicateElementsArray = Handler.readData(Handler.streamFile("unittest/duplicateElemsMedium.TestData.in"), 
+        TestData::from); 
+        TestData[] expectedArray = Handler.readData(Handler.streamFile("unittest/duplicateElemsMedium.TestData.out"), 
+        TestData::from); 
+        LevelSort.sort(duplicateElementsArray, 1);
+        assertArrayEquals(expectedArray, duplicateElementsArray);
+    }  
+
+    @Test
+    void givenDuplicateElementsArrayMedium_whenLevelSort_thenReturnNumberOfCompares() {
+        TestData[] duplicateElementsArray = Handler.readData(Handler.streamFile("unittest/duplicateElemsMedium.TestData.in"), 
+        TestData::from); 
+        assertEquals(27, LevelSort.sort(duplicateElementsArray, 3));
+    } 
+
+
 }
