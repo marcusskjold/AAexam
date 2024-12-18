df6985cc23928c0574be5b03bbfcf283b798ccda
Marcus Skjold <marcusskjold@gmail.com>
2024-12-18 22:12:47 +0100

    Implement twoSequenceSelect in MergeParallel - lacks test

--- AAexam/app/src/main/java/sorting/TwoSequenceSelect.java
+++
@@ -1,9 +0,0 @@
-package sorting;
-
-public class TwoSequenceSelect {
-    
-    public static <T extends Comparable<? super T>> void twoSequenceSelect(T[] a, T[] b, int k) {
-        int la = a.length, lb = b.length;
-        if (k < 0 || k > la + lb) throw new IllegalArgumentException( "k is out of bounds");
-    }
-}
