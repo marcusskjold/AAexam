7589a61412020da692b75bcaa9478982e530c07e
Marcus Skjold <marcusskjold@gmail.com>
2024-12-19 04:35:17 +0100

    Got stuck because of parallel visibility issues

---
+++ AAexam/app/src/test/java/sorting/FullRecursiveMergeSortParallelTest.java
@@ -0,0 +1,183 @@
+package sorting;
+
+import static data.Handler.randomString;
+import static data.Handler.generate;
+import static data.Handler.randomize;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static sorting.Util.isSorted;
+import static sorting.FullRecursiveMergeSortParallel.sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import data.Handler;
+import data.TestData;
+
+public class FullRecursiveMergeSortParallelTest {
+
+    // ====================================
+    // String tests
+    // ====================================
+
+    @Disabled
+    @Test void givenRandomStrings_whenSort_thenSorted() {
+        int n = 100, m = 20;
+        String[] in = generate(n, i -> randomString(m));
+        assertFalse(isSorted(in));
+        sort(in, 10);
+        //System.out.println(Arrays.toString(in));
+        assertTrue(isSorted(in));
+    }
+
+    //-----------------------------------
+    //Case: Empty array
+    //-----------------------------------
+    @Disabled
+    @Test void givenEmptyArray_whenMergeSort_thenEmptyArray() {
+        Integer[] emptyArray = new Integer[0];
+        sort(emptyArray, 4);
+        assertArrayEquals(new Integer[]{}, emptyArray);
+    }
+
+    @Disabled
+    @Test void givenEmptyArray_whenMergeSort_thenReturnZeroCompares() {
+        Integer[] emptyArray = new Integer[0];
+        assertEquals(0, sort(emptyArray, 4));
+    }
+
+    //-----------------------------------
+    //Case: Only one element
+    //-----------------------------------
+    @Disabled
+    @Test void givenSizeOneArray_whenMergeSort_thenIdenticalArray() {
+        Integer[] sizeOneArray = new Integer[]{1};
+        sort(sizeOneArray, 4);
+        assertArrayEquals(new Integer[]{1}, sizeOneArray);
+    }
+
+    @Disabled
+    @Test void givenSizeOneArray_whenMergeSort_thenReturnZeroCompares() {
+        Integer[] sizeOneArray = new Integer[]{1};
+        assertEquals(0, sort(sizeOneArray, 4));
+    }
+
+    //-----------------------------------
+    //Case: Descending order rather than ascending
+    //-----------------------------------
+    @Test void givenDescendingArray_whenMergeSort_thenAscendingArray() {
+        Integer[] a = new Integer[]{2,1,0,-1};
+        sort(a, 4);
+        System.out.println(Arrays.toString(a));
+        assertArrayEquals(new Integer[]{-1,0,1,2}, a);
+    }
+
+    @Disabled
+    @Test void givenDescendingArray_whenMergeSort_thenReturnNumberOfCompares() {
+        Integer[] descendingArray = new Integer[]{2,1,0,-1};
+        assertEquals(4, sort(descendingArray, 4));
+    }
+
+    //-----------------------------------
+    //Case: Odd amount of elements
+    //-----------------------------------
+    @Disabled
+    @Test void givenOddSizeArray_whenMergeSort_thenSortsArray() {
+        Integer[] oddSizeArray = new Integer[]{9,1,2,0,7,4,3,8,5,6,10};
+        sort(oddSizeArray, 4);
+        assertArrayEquals(new Integer[]{0,1,2,3,4,5,6,7,8,9,10}, oddSizeArray);
+    }
+
+    @Disabled
+    @Test void givenOddSizeArray_whenMergeSort_thenReturnNumberOfCompares() {
+        Integer[] oddSizeArray = new Integer[]{9,1,2,0,7,4,3,8,5,6,10};
+        assertEquals(29, sort(oddSizeArray, 4));
+    }
+
+
+    //-----------------------------------
+    //Case: all elements equal value (using Testdata tuples with identical values and distinct id's)
+    //As such, if not identical would imply that some tuples with equal value were switched (e.g. try value = -i)
+    //-----------------------------------
+    @Disabled
+    @Test void givenIdenticalElementsArray_whenMergeSort_thenPreserveStability() {
+        TestData[] identicalElementsArray = generate(10, i -> new TestData(i,0));
+        sort(identicalElementsArray, 4);
+        assertArrayEquals(generate(10, i -> new TestData(i,0)), identicalElementsArray);
+    }
+
+    @Disabled
+    @Test void givenIdenticalElementsArray_whenMergeSort_thenReturnNumberOfCompares() {
+        TestData[] identicalElementsArray = generate(10, i -> new TestData(i,0));
+        assertEquals(19, sort(identicalElementsArray, 6));
+    }
+
+    //-----------------------------------
+    //Case: small array contains duplicates (for easier readability compared to medium array)
+    //-----------------------------------
+    @Disabled
+    @Test void givenDuplicateElementsArray_whenMergeSort_thenSortsArrayStably() {
+        TestData[] duplicateElementsArray = Handler.readData(Handler.streamFile("unittest/duplicateElems.TestData.in"), 
+        TestData::from); 
+        TestData[] expectedArray = Handler.readData(Handler.streamFile("unittest/duplicateElems.TestData.out"), 
+        TestData::from); 
+        sort(duplicateElementsArray, 4);
+        assertArrayEquals(expectedArray, duplicateElementsArray);
+    }
+
+    @Disabled
+    @Test void givenDuplicateElementsArray_whenMergeSort_thenReturnNumberOfCompares() {
+        TestData[] duplicateElementsArray = Handler.readData(Handler.streamFile("unittest/duplicateElems.TestData.in"), 
+        TestData::from); 
+        assertEquals(8, sort(duplicateElementsArray, 4));
+    }
+
+    //-----------------------------------
+    //Case: medium array contains duplicates
+    //-----------------------------------
+    //TODO: Important! When one of the files are modified, it doesn't seem to be updated in the read array (a cache seems to be used) (Maybe okay and because it has to be built also?)
+    //changing name and back can solve this ish
+    @Disabled
+    @Test void givenDuplicateElementsArrayMedium_whenMergeSort_thenSortsArrayStably() {
+        TestData[] duplicateElementsArray = Handler.readData(Handler.streamFile("unittest/duplicateElemsMedium.TestData.in"), 
+        TestData::from); 
+        TestData[] expectedArray = Handler.readData(Handler.streamFile("unittest/duplicateElemsMedium.TestData.out"), 
+        TestData::from); 
+
+        // Debug: Log input and expected output
+        //System.out.println("Input Array: " + Arrays.toString(duplicateElementsArray));
+        //System.out.println("Expected Array: " + Arrays.toString(expectedArray));
+
+        sort(duplicateElementsArray, 4);
+
+        // Debug: sorted input
+        //System.out.println("Sorted Array: " + Arrays.toString(duplicateElementsArray));
+
+        assertArrayEquals(expectedArray, duplicateElementsArray);
+    }    
+
+    @Disabled
+    @Test void givenDuplicateElementsArrayMedium_whenMergeSort_thenReturnNumberOfCompares() {
+        TestData[] duplicateElementsArray = Handler.readData(Handler.streamFile("unittest/duplicateElemsMedium.TestData.in"), 
+        TestData::from); 
+
+        assertEquals(23, sort(duplicateElementsArray, 4));
+    }    
+
+    @Disabled
+    @RepeatedTest(200) void givenLargeArray_whenSort_thenIdenticalResultToSequential() {
+        Integer[] actual   = randomize(generate(10_000, i -> i));
+        Integer[] expected = actual.clone();
+        TopDownMergeSort.sort(expected);
+        sort(actual, 32);
+        assertArrayEquals(expected, actual);
+        
+    }
+
+}
--- AAexam/app/src/test/java/sorting/MergeParallelTest.java
+++ AAexam/app/src/test/java/sorting/MergeParallelTest.java
@@ -18,14 +18,14 @@ public class MergeParallelTest {
     //-----------------------------------
     @Test void givenEvenSubarrays_whenMerge_thenMerge() {
         Integer[] a   = new Integer[]{3,4,1,2};
-        Integer[] aux = new Integer[4];
+        Integer[] aux = a.clone();
         merge(a, aux, 0, 1, 3, 2);
-        assertArrayEquals(aux, new Integer[]{1,2,3,4});
+        assertArrayEquals(a, new Integer[]{1,2,3,4});
     }
 
     @Test void givenEvenSubarrays_whenMerge_thenReturnNumberOfCompares() {
         Integer[] a   = new Integer[]{3,4,1,2};
-        Integer[] aux = new Integer[4];
+        Integer[] aux = a.clone();
         assertEquals(2, merge(a, aux, 0, 1, 3, 2));
     }
 
@@ -38,12 +38,13 @@ public class MergeParallelTest {
         Integer[] aux = a.clone();
         Integer[] expected = new Integer[]{7,2,3,4,5,6,5,4,3,2,1};
         merge(a, aux, 1, 3, 5, 2);
-        assertArrayEquals(expected, aux);
+        assertArrayEquals(expected, a);
     }
 
     @Test void givenUnevenSubarrays_whenMerge_thenReturnNumberOfCompares() {
         Integer[] a = new Integer[]{7,2,3,5,4,6,5,4,3,2,1};
-        int compares = merge(a, new Integer[11], 1, 3, 5, 2);
+        Integer[] aux = a.clone();
+        int compares = merge(a, aux, 1, 3, 5, 2);
         assertEquals(4, compares);
     }
 
@@ -55,15 +56,16 @@ public class MergeParallelTest {
             Handler.streamFile("unittest/duplicateInSubarrays.TestData.in"), TestData::from); 
         TestData[] expected = Handler.readData(
             Handler.streamFile("unittest/duplicateInSubarrays.TestData.out"), TestData::from); 
-        TestData[] aux =  new TestData[8];
+        TestData[] aux = a.clone();
         merge(a, aux, 0, 3, 7, 2);
-        assertArrayEquals(expected, aux);
+        assertArrayEquals(expected, a);
     }
 
     @Test void givenDuplicateElements_whenMerge_thenReturnNumberOfCompares() {
         TestData[] a = Handler.readData(
             Handler.streamFile("unittest/duplicateInSubarrays.TestData.in"), TestData::from); 
-        int compares = merge(a, new TestData[8], 0, 3, 7, 2);
+        TestData[] aux = a.clone();
+        int compares = merge(a, aux, 0, 3, 7, 2);
         assertEquals(7, compares);
     }
 }
